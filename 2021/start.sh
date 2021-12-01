e578f1793ebd4197f31dca9d4d3f51320dfc0c05
mellonnen <arvidgotthard98@gmail.com>
2021-12-01 09:07:24 +0100

    updated start script

--- 2021/start.sh
+++ 2021/start.sh
@@ -1,18 +1,34 @@
-#!/bin/zsh
+#!/bin/sh
 
+# Get day from date.
 day=$(date | awk '{ print $3 }')
-mkdir "day$day"
 
+# check if we have a single digit day number.
+if [ $(($day+0)) -lt 10 ]
+then
+  dir="day0$day"
+else
+  dir="day$day"
+fi
+
+# create directory.
+mkdir "$dir"
+
+# Get input.
 curl "https://adventofcode.com/2021/day/$day/input" \
   -H 'authority: adventofcode.com' \
   -H "cookie: session=$(cat ./.cookie.txt)" \
   --compressed \
-  -o "day$day/input"
+  -o "$dir/input"
 
-cd "day$day" 
+cd "$dir" 
 
+# init rust project.
 cargo init --bin --vcs=none
 
+# add dependecy.
+# NOTE: library has some annoying issues, might wanna find something better.
 echo 'easy_io = "0.3.0"' >> Cargo.toml
 
+# open problem description in browser.
 wsl-open "https://adventofcode.com/2021/day/$day"
