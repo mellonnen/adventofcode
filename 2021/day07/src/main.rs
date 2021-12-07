dfa3e9f0262e18b589cf322cc78378b1e116015d
mellonnen <arvidgotthard98@gmail.com>
2021-12-07 07:40:22 +0100

    2021: day 7 (refactor)

--- 2021/day07/src/main.rs
+++ 2021/day07/src/main.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 fn main() {
     let in_str = include_str!("../input");
     println!("PART1\n=====\n");
@@ -11,53 +9,38 @@ fn main() {
 // O(n * r), where n = number of crabs, and r is the length of the range (max(crabs) - min(crabs))
 fn part1(input: &str) -> i64 {
     // parse input, and sort
-    let mut crabs: Vec<i64> = input
+    let crabs: Vec<i64> = input
         .trim()
         .split(",")
         .map(|s| s.parse().unwrap())
         .collect();
-    crabs.sort();
 
     // loop over the range to minimize the total fuel
     let mut fuel = i64::MAX;
-    let start = *crabs.first().unwrap();
-    let stop = *crabs.last().unwrap();
+    let start = *crabs.iter().min().unwrap();
+    let stop = *crabs.iter().max().unwrap();
     for i in start..stop {
         let f = crabs.iter().map(|c| (c - i).abs()).sum();
         fuel = fuel.min(f);
     }
     fuel
 }
-// O(n * r^2)
-//NOTE: This is the worst case, amortized it is probably better due to the memoization.
+// O(n * r)
 fn part2(input: &str) -> i64 {
-    let mut crabs: Vec<i64> = input
+    let crabs: Vec<i64> = input
         .trim()
         .split(",")
         .map(|s| s.parse().unwrap())
         .collect();
-    crabs.sort();
 
     let mut fuel = i64::MAX;
-    let start = *crabs.first().unwrap();
-    let stop = *crabs.last().unwrap();
-    // memoize the fuel cost for different distances.
-    let mut memo: HashMap<i64, i64> = HashMap::new();
-
+    let start = *crabs.iter().min().unwrap();
+    let stop = *crabs.iter().max().unwrap();
     for i in start..stop {
         let mut f = 0;
         for c in &crabs {
             let d = (c - i).abs();
-
-            // check if we already have calculated this cost.
-            if let Some(x) = memo.get(&d) {
-                f += x;
-            } else {
-                // calculate and memoize cost.
-                let x: i64 = (1..(d + 1)).sum();
-                f += x;
-                memo.insert(d, x);
-            }
+            f += d * (d + 1) / 2
         }
         fuel = fuel.min(f);
     }
